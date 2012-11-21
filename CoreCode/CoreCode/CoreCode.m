673bedc914b8959862737d78fdbd58727addb633
mengpeng <ampy.apple@gmail.com>
2012-11-21 10:34:30 +0800

    1.0

---
+++ CoreCode/CoreCode/CoreCode.m
@@ -0,0 +1,25 @@
+//
+//  CoreCode.m
+//  CoreCode
+//
+//  Created by  rtsafe02 on 12-10-24.
+//  Copyright (c) 2012年 ampy. All rights reserved.
+//
+
+#import "CoreCode.h"
+
+@implementation CoreCode
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+- (void)TestShow
+{
+    NSLog(@"TestFramework");
+}
+@end
